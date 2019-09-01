4f3b25016fc5affc4b9414f548734d101fd66886
Juan.Rodriguez <juan.rodriguez1832@gmail.com>
2019-09-01 11:20:25 -0400

    Mi tercer Guardado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BodyComponent } from './component/body/body.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:  'app-healder',
+    templateUrl: './header.component.html'
+})
+
+export class HeaderComponent {
+
+}
+
