ef832544d5811462aa40b1553a5ea8cbd506b027
Juan.Rodriguez <juan.rodriguez1832@gmail.com>
2019-09-01 12:26:50 -0400

    Lección 40 de Angular

--- src/app/component/body/body.component.ts
+++ src/app/component/body/body.component.ts
@@ -6,5 +6,11 @@ templateUrl: './body.component.html'
 })
 
 export class BodyComponent {
+    mostrar = true;
+    frase: any = {
+        mensaje: 'Un gran popder requiere una gran responsabilidad',
+        autor:  'Ben Parker'
+        };
 
+        personajes: string [] = ['Juan', 'Cley', 'Genesis', 'Marco'];
 }
